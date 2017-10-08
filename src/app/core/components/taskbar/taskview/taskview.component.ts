c7369e7c787a707aa22cff1377d19da140ffe1ba
Chris Daniel <cwdaniel@fuse.net>
2017-10-07 22:38:02 -0400

    Added Segoe MDL2 icons over images
    Meaning, users on non-windows will see lame boxes. Those users should look for the icons and import them. I dont know if I can legally link those soooo I wont.

--- src/app/core/components/taskbar/taskview/taskview.component.ts
+++ src/app/core/components/taskbar/taskview/taskview.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-   selector: 'taskbar-taskview',
-   templateUrl: './taskview.component.html',
-   styleUrls: ['./taskview.component.css']
+    selector: 'taskbar-taskview',
+    templateUrl: './taskview.component.html',
+    styleUrls: ['./taskview.component.css']
 })
 export class TaskviewComponent implements OnInit {
+    public isTaskViewActive = false;
+    constructor() { }
 
-   constructor() { }
-
-   ngOnInit() {
-   }
-
+    ngOnInit() {
+    }
+    
+    public taskViewLeave = () => this.isTaskViewActive = false;
+    public taskViewEnter = () => this.isTaskViewActive = true;
+    public getTaskViewFont = () => this.isTaskViewActive ? '&#xEB91;' : '&#xE7C4;';
 }
--- src/app/store/app.reducers.ts
+++ src/app/store/app.reducers.ts
@@ -3,9 +3,20 @@ import { ActionReducerMap } from '@ngrx/store';
 import * as fromAuth from '../auth/store/auth.reducers';
 
 export interface AppState {
-  auth: fromAuth.State
+  auth: fromAuth.State;
 }
+export const uiState11: UiState = {
+  defaultApps: [{ title: 'string' }],
+};
 
 export const reducers: ActionReducerMap<AppState> = {
-  auth: fromAuth.authReducer
+  auth: fromAuth.authReducer,
 };
+
+export interface UiState {
+  defaultApps: DefaultApps[];
+}
+
+export interface DefaultApps {
+  title: string;
+}
